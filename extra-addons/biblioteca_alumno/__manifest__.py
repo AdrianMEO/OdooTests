f321a607ed755a9d379afeb240d30d5e1ce51e3b
Martínez de Eguílaz Orrico <adrian.martinez11@isepceu.es>
2025-12-12 16:35:19 +0100

    Añadir módulo Biblioteca Alumno

---
+++ extra-addons/biblioteca_alumno/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Gestión sencilla de libros (práctica DAM)",
+    "description": """
+Módulo de ejemplo para la asignatura de Empresa / Sistemas de Gestión Empresarial.
+Permite gestionar libros de una biblioteca: título, autor, fecha, páginas y disponibilidad.
+""",
+    "author": "Adrián Martínez",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": [
+        "views/biblioteca_libro_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
+
